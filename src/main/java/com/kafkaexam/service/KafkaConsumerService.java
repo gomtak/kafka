8987d8a697b030cb72e455298be8112b7e83b096
tak6831 <tak6831@gmail.com>
2022-05-25 00:58:26 +0900

    docs : remove help.md

--- src/main/java/com/kafkaexam/service/KafkaConsumerService.java
+++ src/main/java/com/kafkaexam/service/KafkaConsumerService.java
@@ -1,7 +1,5 @@
 package com.kafkaexam.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
